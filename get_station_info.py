a3890acfd81f7742812047858d026bc60c2ad5e7
Thomas Dimperio <twdimperio@gmail.com>
2026-09-01 12:52:22 +0200

    Add db connection, populate station_information table

--- get_station_info.py
+++ get_station_info.py
@@ -1,5 +1,5 @@
 import requests
-import json
+from db import get_connection
 
 URL = 'https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_bn/en/station_information.json'
 
@@ -8,4 +8,41 @@ response.raise_for_status()
 
 data = response.json()
 
-print(data['data']['stations'])
+stations = data['data']['stations']
+
+with get_connection() as conn:
+    with conn.cursor() as cur:
+        for station in stations:
+            cur.execute(
+                """
+                INSERT INTO station_information (
+                    station_id,
+                    name,
+                    short_name,
+                    lat,
+                    lon,
+                    region_id,
+                    is_virtual_station,
+                    capacity
+                    )
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (station_id) DO UPDATE SET
+                    name = EXCLUDED.name,
+                    short_name = EXCLUDED.short_name,
+                    lat = EXCLUDED.lat,
+                    lon = EXCLUDED.lon,
+                    region_id = EXCLUDED.region_id,
+                    is_virtual_station = EXCLUDED.is_virtual_station,
+                    capacity = EXCLUDED.capacity
+                """,
+                (
+                    station['station_id'],
+                    station['name'],
+                    station['short_name'],
+                    station['lat'],
+                    station['lon'],
+                    station['region_id'],
+                    station['is_virtual_station'],
+                    station.get('capacity')
+                )
+            )
