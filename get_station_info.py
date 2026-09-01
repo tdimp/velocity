8238f68df2e5a58a57a29503b6fdab25b66e034f
Thomas Dimperio <twdimperio@gmail.com>
2026-09-01 17:21:18 +0200

    Refactor get_station_info into functions

--- get_station_info.py
+++ get_station_info.py
@@ -3,46 +3,76 @@ from db import get_connection
 
 URL = 'https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_bn/en/station_information.json'
 
-response = requests.get(URL, timeout=10)
-response.raise_for_status()
 
-data = response.json()
+def get_station_info():
+    response = requests.get(URL, timeout=10)
+    response.raise_for_status()
+    
+    data = response.json()
+    return data['data']['stations']
 
-stations = data['data']['stations']
 
-with get_connection() as conn:
-    with conn.cursor() as cur:
-        for station in stations:
-            cur.execute(
-                """
-                INSERT INTO station_information (
-                    station_id,
-                    name,
-                    short_name,
-                    lat,
-                    lon,
-                    region_id,
-                    is_virtual_station,
-                    capacity
-                    )
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (station_id) DO UPDATE SET
-                    name = EXCLUDED.name,
-                    short_name = EXCLUDED.short_name,
-                    lat = EXCLUDED.lat,
-                    lon = EXCLUDED.lon,
-                    region_id = EXCLUDED.region_id,
-                    is_virtual_station = EXCLUDED.is_virtual_station,
-                    capacity = EXCLUDED.capacity
-                """,
-                (
-                    station['station_id'],
-                    station['name'],
-                    station['short_name'],
-                    station['lat'],
-                    station['lon'],
-                    station['region_id'],
-                    station['is_virtual_station'],
-                    station.get('capacity')
-                )
-            )
+def load_station_info(stations):
+    with get_connection() as conn:
+     with conn.cursor() as cur:
+         for station in stations:
+             cur.execute(
+                 """
+                 INSERT INTO station_information (
+                     station_id,
+                     name,
+                     short_name,
+                     lat,
+                     lon,
+                     region_id,
+                     is_virtual_station,
+                     capacity
+                     )
+                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                 ON CONFLICT (station_id) DO UPDATE SET
+                     name = EXCLUDED.name,
+                     short_name = EXCLUDED.short_name,
+                     lat = EXCLUDED.lat,
+                     lon = EXCLUDED.lon,
+                     region_id = EXCLUDED.region_id,
+                     is_virtual_station = EXCLUDED.is_virtual_station,
+                     capacity = EXCLUDED.capacity
+                 """,
+                 (
+                     station['station_id'],
+                     station['name'],
+                     station['short_name'],
+                     station['lat'],
+                     station['lon'],
+                     station['region_id'],
+                     station['is_virtual_station'],
+                     station.get('capacity')
+                 )
+             )
+
+
+def validate_station_info(stations):
+    if not stations:
+        raise ValueError('No station info retrieved from API.')
+    
+    required_fields = {
+        'station_id',
+        'name',
+        'lat',
+        'lon',
+        'region_id',
+    }
+    
+    for station in stations:
+        missing_fields = required_fields - station.keys()
+        
+        if missing_fields:
+            raise ValueError(f'Station {station["station_id"]} is missing fields: {missing_fields}')
+    
+    return True
+
+
+if __name__ == '__main__':
+    stations = get_station_info()
+    validate_station_info(stations)
+    load_station_info(stations)
